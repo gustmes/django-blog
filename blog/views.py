4c393f9dcb1f4e0454a4867dbccfcf00c7a8e56d
gustmes <gustmes@hotmail.com>
2020-06-16 20:07:21 -0400

    Added base_generic.html and index.html, so index page is now viewable

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from blog.models import Author, Post
+
 # Create your views here.
 def index(request):
-    return "Hello, world!"
+    num_posts = Post.objects.count()
+    num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        "num_posts": num_posts,
+        "num_authors": num_authors,
+        "num_visits": num_visits,
+    }
+
+    return render(request, 'blog/index.html', context=context)
